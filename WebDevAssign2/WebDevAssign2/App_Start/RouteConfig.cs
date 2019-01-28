f747233533f786d7d98572aab06e9239db918b56
DESKTOP-UKSEOO1\Ryan Slack <ryanruler@hotmail.com>
2019-01-28 18:25:59 +1300

    Up to Lab 6a Completed

--- WebDevAssign2/WebDevAssign2/App_Start/RouteConfig.cs
+++ WebDevAssign2/WebDevAssign2/App_Start/RouteConfig.cs
@@ -14,6 +14,30 @@ namespace WebDevAssign2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "ProductsbyCategoryByPage",
+              url: "Products/{category}/Page{page}",
+              defaults: new { controller = "Products", action = "Index" }
+              );
+
+            routes.MapRoute(
+               name: "ProductsbyPage",
+               url: "Products/Page{page}",
+               defaults: new { controller = "Products", action = "Index" }
+               );
+
+            routes.MapRoute(
+                name: "ProductsByCategory",
+                url: "Products/{category}",
+                defaults: new { controller = "Products", action = "Index" }
+                );
+
+            routes.MapRoute(
+              name: "ProductsIndex",
+              url: "Products",
+              defaults: new { controller = "Products", action = "Index" }
+              );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
